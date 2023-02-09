8528f59bedd9a4e580fbc6d3a98379b4ef22faf7
unknown <taufikfadillah2003@gmail.com>
2023-02-09 23:38:30 +0700

    portfolio tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        xl: "4rem",
+      },
+    },
+    extend: {
+      fontFamily: {
+        inter: ["Inter"],
+        logo: ["Kalam"],
+      },
+      colors: {
+        primary: "#0B1723",
+      },
+    },
+  },
+  plugins: [],
+};
